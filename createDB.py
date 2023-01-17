bef6be9c4e332e4c4c9b1703e44c6f9b728387a3
Magdalena <magdawnowicka@gmail.com>
2023-01-17 17:58:35 +0100

    add script for creating db

---
+++ createDB.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sqlite3
+import time
+import os
+
+
+def create_database():
+    if os.path.exists("players.db"):
+        os.remove("players.db")
+        print("An old database removed.")
+    connection = sqlite3.connect("players.db")
+    cursor = connection.cursor()
+    cursor.execute(""" CREATE TABLE score_board (
+        rfid text,
+        date_of_game text,
+        score integer,
+        time_in_seconds integer 
+    )""")
+    connection.commit()
+    connection.close()
+    print("The players database created.")
+
+
+if __name__ == "__main__":
+    create_database()
